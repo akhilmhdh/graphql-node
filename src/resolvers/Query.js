6e9c381a48b89d7387820b6c4a69f19d00bfd796
akhilmhdh <akhilmhdh@gmail.com>
2021-05-21 16:30:39 +0530

    feat: completed query arguments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,24 @@
-export const feed = (parent, args, context, info) => context.prisma.link.findMany();
+export const feed = async (parent, args, context, info) => {
+    const where = args.filter
+        ? {
+              OR: [{ description: { contains: args.filter } }, { url: { contains: args.url } }],
+          }
+        : {};
+
+    const links = await context.prisma.link.findMany({
+        where,
+        skip: args.skip,
+        take: args.limit,
+        orderBy: args.orderBy,
+    });
+
+    const count = await context.prisma.link.count({ where });
+
+    return {
+        links,
+        count,
+    };
+};
 
 export const info = () => "Something graphql trail!!";
 

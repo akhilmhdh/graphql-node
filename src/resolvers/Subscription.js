779f67ad8a3205786ba2129823789bd832fff13d
akhilmhdh <akhilmhdh@gmail.com>
2021-05-21 14:51:45 +0530

    feat: added new vote subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -2,11 +2,21 @@ const newLinkSubscribe = (parent, args, context, info) => {
     return context.pubsub.asyncIterator("NEW_LINK");
 };
 
+const newVoteSubscribe = (parent, args, context, info) => {
+    return context.pubsub.asyncIterator("NEW_VOTE");
+};
+
 const newLink = {
     subscribe: newLinkSubscribe,
     resolve: (payload) => payload,
 };
 
+const newVote = {
+    subscribe: newVoteSubscribe,
+    resolve: (payload) => payload,
+};
+
 export default {
     newLink,
+    newVote,
 };
